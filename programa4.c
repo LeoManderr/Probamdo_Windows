c4d611aaac1ef25a56827979d9c521e4d0548266
unknown <leonardojosemalave@gmail.com>
2022-10-07 19:08:49 -0300

    first commit

---
+++ programa4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main () {
+
+    int lado, perimetro;
+    printf("Ingrese el lado del cuadrado");
+    scanf("%i", &lado);
+    perimetro = lado*4;
+    printf("El perimetro del cuadrado es: ");
+    printf("%i", perimetro);
+    getch();
+    
+
+
+
+}
